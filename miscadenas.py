5c8f14676b405f7ab7612eac57e2684600e2dd21
Camila-rodriguezz <a.22308051281300@cbtis128.edu.mx>
2024-09-06 09:32:28 -0600

    Add files via upload

---
+++ miscadenas.py
@@ -0,0 +1,21 @@
+"""Camila Rodriguez. Trabajando con cadenas"""
+
+nombre="Camila"
+print(nombre[0])
+print(nombre)
+for c in "Camila":
+  print (c)
+
+misnovias=["Agripina","Anastacia", "maria"]
+for chicas in misnovias:
+    print(chicas)
+print("otro ejemplo") 
+b = "Camila Rodriguez"
+print(b[2:5]) 
+a = "Camila Rodriguez"
+print(a.upper())
+print("Concatenar")
+a = "Maria"
+b = "Perez"
+c = a +" "+b
+print(c)
